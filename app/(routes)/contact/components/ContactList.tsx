e9cb5e9833e63bf15ad0b637ba8db404c7e6f6c7
JackSlice2 <jackslice9@gmail.com>
2024-04-16 11:42:28 -0300

    Icons and styling

--- app/(routes)/contact/components/ContactList.tsx
+++ app/(routes)/contact/components/ContactList.tsx
@@ -1,14 +1,14 @@
 import DiscountButton from '@/app/components/DiscountButton'
-import { Mail, Phone } from 'lucide-react'
 import React from 'react'
+import { FaWhatsapp, FaTelegramPlane, FaLock, FaEnvelope } from 'react-icons/fa';
 
 const ContactInfo = [
-    { icon: <Phone />, label: 'WhatsApp', info: '+55 51 98559-0254', url: 'https://wa.me/5551985590254'},
-    { icon: <Phone />, label: 'WhatsApp', info: '+55 51 98579-0398', url: 'https://wa.me/5551985790398'},
-    { icon: <Phone />, label: 'Telegram', info: '+55 51 98559-0254', url: 'https://t.me/AfroTech'},
-    { icon: <Phone />, label: 'Signal', info: '+55 51 98559-0254', url: 'https://signal.org/'},
-    { icon: <Mail />, label: 'Email', info: 'AfroTech@proton.me', url: 'mailto'},
-]
+    { icon: <FaWhatsapp />, label: 'WhatsApp', info: '+55 51 98559-0254', url: 'https://wa.me/5551985590254'},
+    { icon: <FaWhatsapp />, label: 'WhatsApp', info: '+55 51 98579-0398', url: 'https://wa.me/5551985790398'},
+    { icon: <FaTelegramPlane />, label: 'Telegram', info: '+55 51 98559-0254', url: 'https://t.me/AfroTech'},
+    { icon: <FaLock />, label: 'Signal', info: '+55 51 98559-0254', url: 'https://signal.org/'},
+    { icon: <FaEnvelope />, label: 'Email', info: 'AfroTech@proton.me', url: 'mailto'},
+];
 
 const ContactList = () => {
  return (
@@ -17,7 +17,7 @@ const ContactList = () => {
         <a href={item.url}  key={index} target='_blank' >
         <div className="flex items-center p-4 rounded-xl bg-blue-400/20 hover:bg-blue-900/30 hover:text-blue-500 shadow-md cursor-pointer">
           <div className="w-10 h-10 bg-blue-500 text-white flex justify-center items-center rounded-full">
-            {React.cloneElement(item.icon, { size: 24, fill: "currentColor" })}
+            {React.cloneElement(item.icon, { size: 24 })}
           </div>
           <div className="ml-4 p-1">
             <h3 className='text-lg font-semibold'>{item.label}</h3>
--- app/(routes)/services/components/Services.tsx
+++ app/(routes)/services/components/Services.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import Navbar from '../../../components/Navbar';
-import { Phone } from 'lucide-react';
 import Link from 'next/link';
+import { Home, Globe, Database, Clipboard, Wrench, AppWindow } from 'lucide-react';
 
 const ServiceInfo = [
- { icon: <Phone />, label: 'Landing Page', 
-  description: 'A beautifully designed landing page to showcase your brand.A beautifully designed landing page to showcase your brand.', 
-    url: '/services/pages/landingPage' },
- { icon: <Phone />, label: 'Websites', 
-  description: 'Custom-built websites tailored to your business needs.Custom-built websites tailored to your business needs.', 
-    url: '/services/pages/websites' },
- { icon: <Phone />, label: 'WebApps', 
-  description: 'Innovative web applications to enhance your online presence.Innovative web applications to enhance your online presence.', 
-    url: '/services/pages/webApps' },
- { icon: <Phone />, label: 'Databases', 
-  description: 'Secure and efficient database solutions for your data storage needs.Secure and efficient database solutions for your data storage needs.', 
-    url: '/services/pages/databases' },
- { icon: <Phone />, label: 'CRMs e CRUDs', 
-  description: 'Comprehensive CRM and CRUD solutions for streamlined operations.Comprehensive CRM and CRUD solutions for streamlined operations.', 
-    url: '/services/pages/crm-cruds' },
- { icon: <Phone />, label: 'Manutenção', 
-  description: 'Reliable maintenance services to keep your systems running smoothly.Reliable maintenance services to keep your systems running smoothly.', 
-    url: '/services/pages/maintenance' },
+ { icon: <Home />, label: 'Landing Page', description: 'A beautifully designed landing page to showcase your brand.', url: '/services/pages/landingPage' },
+ { icon: <Globe />, label: 'Websites', description: 'Custom-built websites tailored to your business needs.', url: '/services/pages/websites' },
+ { icon: <AppWindow />, label: 'WebApps', description: 'Innovative web applications to enhance your online presence.', url: '/services/pages/webApps' },
+ { icon: <Database />, label: 'Databases', description: 'Secure and efficient database solutions for your data storage needs.', url: '/services/pages/databases' },
+ { icon: <Clipboard />, label: 'CRMs e CRUDs', description: 'Comprehensive CRM and CRUD solutions for streamlined operations.', url: '/services/pages/crm-cruds' },
+ { icon: <Wrench />, label: 'Manutenção', description: 'Reliable maintenance services to keep your systems running smoothly.', url: '/services/pages/maintenance' },
 ];
+
 
 const Services = () => {
  return (
@@ -31,7 +20,7 @@ const Services = () => {
     <div className="h-full w-full md:flex-row lg:flex-row xl:flex justify-center p-1">
       <div className="font-medium text-center">
         <h1 className='text-4xl'>
-          Our Services
+          Serviços que Prestamos
         </h1>
         <div className='my-10'>
         <p className='p-5 text-xl bg-blue-800/20 rounded-2xl'>
@@ -39,15 +28,15 @@ const Services = () => {
         </p>
         </div>
         <div 
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10 m-15 justify-center items-center">
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10 pb-10 m-15 justify-center items-center">
           {ServiceInfo.map((item, index) => (
             <Link href={item.url} key={index} 
             className="bg-blue-900/20 rounded-3xl p-6 py-10 hover:bg-blue-800/40 transition-shadow duration-300 cursor-pointer max-w-96 min-h-[200px] flex flex-col justify-between hover:scale-110">
               <div className="flex justify-center text-blue-500">
-              {React.cloneElement(item.icon, { size: 30, fill: "currentColor" })}
+              {React.cloneElement(item.icon, { size: 50 })}
               </div>
               <div>
-              <h2 className="text-lg font-semibold my-4">{item.label}</h2>
+              <h2 className="text-lg font-semibold my-2">{item.label}</h2>
               <p className="text-gray-600 my-2">{item.description}</p>
               </div>
             </Link>
