73e6f50ca272229fa6232c9dcfc11ec245f2c7b7
JackSlice2 <jackslice9@gmail.com>
2024-04-16 11:56:26 -0300

    styling

--- app/(routes)/services/pages/landingPage/LandingPage.tsx
+++ app/(routes)/services/pages/landingPage/LandingPage.tsx
@@ -33,9 +33,9 @@ const LandingPage = () => {
     return (
         <div className="flex flex-col justify-center items-center px-4 py-8 md:px-8">
       <Navbar />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto p-4 sm:px-6 lg:px-8 rounded-3xl bg-gray-100/10">
         <div className="text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight text-gray-200 sm:text-5xl md:text-6xl">
+          <h1 className="text-4xl font-extrabold tracking-tight text-gray-200 sm:text-5xl md:text-6xl pt-2">
             Landing Page
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-300 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
@@ -43,9 +43,9 @@ const LandingPage = () => {
           </p>
         </div>
 
-        <div className="mt-12 grid gap-10 max-w-2xl mx-auto lg:max-w-none">
+        <div className="mt-5 grid gap-10 max-w-2xl mx-auto lg:max-w-none p-10">
           {LandingPageInfo.map((item, index) => (
-            <div key={index} className={`flex  hover:scale-125 transition flex-col md:flex-row rounded-xl shadow-lg overflow-hidden ${index % 2 === 0 ? '' : 'md:flex-row-reverse'}`}>
+            <div key={index} className={`flex  hover:scale-110 transition flex-col md:flex-row rounded-2xl shadow-lg overflow-hidden ${index % 2 === 0 ? '' : 'md:flex-row-reverse'}`}>
               <div className="flex-shrink-0 md:w-1/3 transition">
                 <img className="h-full w-full object-cover md:w-full" src={item.img} alt={item.title} />
               </div>
