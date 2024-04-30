aca723a907d6f7363f90ffed5bf08e14c46dec52
JackSlice2 <jackslice9@gmail.com>
2024-04-30 19:39:14 -0300

    ? removed

--- app/data/subServicesData/GoogleAds.tsx
+++ app/data/subServicesData/GoogleAds.tsx
@@ -6,16 +6,16 @@ export const GoogleAdsInfo = [
    {
       title: 'Quais são os benefícios de usar o Google Ads?',
       description: 'ALCANCE ESPECÍFICO: Você pode direcionar seus anúncios para um público-alvo específico, o que aumenta as chances de atrair clientes interessados no que você oferece. PAGAMENTO POR DESEMPENHO: Você paga somente quando alguém interage com seu anúncio, seja clicando nele ou visualizando-o mil vezes. Isso garante que você gaste seu dinheiro de forma mais eficaz. CONTROLE DO ORÇAMENTO: Você decide quanto deseja gastar em suas campanhas diariamente e pode ajustar esse valor conforme necessário. DADOS SIMPLES DE ENTENDER: O Google Ads fornece métricas fáceis de entender, como o número de cliques e o custo por clique, para que você possa acompanhar facilmente o desempenho de suas campanhas.',
-      img: '/img/Quais são os benefícios de usar o Google Ads?.jpeg',
+      img: '/img/Quais são os benefícios de usar o Google Ads.jpeg',
    },
    {
       title: 'Como o Google Ads pode ajudar a minha empresa?',
       description: 'O Google Ads desempenha um papel crucial em impulsionar a presença online da sua empresa. Ao direcionar anúncios para pessoas que estão ativamente procurando por produtos ou serviços como os seus, ele aumenta significativamente a visibilidade da sua marca. Essa estratégia inteligente coloca sua empresa na frente de clientes em potencial no momento exato em que estão mais propensos a realizar uma compra. Além disso, o Google Ads oferece a flexibilidade de controlar o orçamento da sua campanha, permitindo que você ajuste seus gastos de acordo com suas necessidades e metas de negócios. Com métricas detalhadas de desempenho, você pode acompanhar o progresso das suas campanhas em tempo real e ajustar sua estratégia conforme necessário. Em última análise, o Google Ads oferece uma maneira mensurável e eficaz de aumentar a visibilidade da sua empresa, gerar tráfego qualificado para o seu site e impulsionar o crescimento dos seus negócios.',
-      img: '/img/Como o Google Ads pode ajudar a minha empresa?.jpeg',
+      img: '/img/Como o Google Ads pode ajudar a minha empresa.jpeg',
    },
    {
       title: 'Quanto tempo leva para ver resultados com o Google Ads?',
       description: 'O tempo necessário para ver resultados com o Google Ads pode variar de acordo com diversos fatores, como o tipo de campanha, o mercado-alvo, a concorrência e a qualidade dos anúncios. Em geral, alguns resultados iniciais podem ser observados logo nas primeiras semanas de lançamento da campanha. No entanto, para alcançar resultados mais significativos e consistentes, geralmente é necessário um período de alguns meses de otimização contínua. Isso permite ajustar a estratégia com base nos dados de desempenho, refinar as palavras-chave e os segmentos de público-alvo e aprimorar a qualidade dos anúncios. Portanto, embora os resultados possam começar a aparecer rapidamente, é importante ter em mente que o sucesso a longo prazo com o Google Ads geralmente requer um compromisso contínuo e paciência para alcançar os objetivos desejados.',
-      img: '/img/Quanto tempo leva para ver resultados com o Google Ads?.jpeg',
+      img: '/img/Quanto tempo leva para ver resultados com o Google Ads.jpeg',
    }
 ];
