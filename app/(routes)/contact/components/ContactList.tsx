f25af13bb8d6759eab4f79428c83a6194d5ddb07
JackSlice2 <jackslice9@gmail.com>
2024-04-30 22:40:02 -0300

    Modal Removed, qrCodes Removed

--- app/(routes)/contact/components/ContactList.tsx
+++ app/(routes)/contact/components/ContactList.tsx
@@ -1,21 +1,7 @@
-import React, { useState } from 'react';
-import { ContactInfo } from '@/app/data/textData/ComponentText';
-import Image from 'next/image';
-import Modal from '@/app/components/Modal';
+import React from 'react'
+import { ContactInfo } from '@/app/data/textData/ComponentText'
 
 const ContactList = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedQRCode, setSelectedQRCode] = useState<string | null>(null); // State to hold the selected QR code
-
-  const openModal = (qrCodeSrc: string) => {
-    setSelectedQRCode(qrCodeSrc); // Set the selected QR code
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
  return (
     <div className="w-full flex flex-col gap-4 px-20 py-5">
       {ContactInfo.map((item, index) => (
@@ -25,26 +11,14 @@ const ContactList = () => {
             {React.cloneElement(item.icon, { size: 24 })}
           </div>
           <div className="ml-4 p-1">
-            <h2 className='font-thin text-gray-300'>{item.type}</h2>
             <h3 className='text-lg font-semibold'>{item.label}</h3>
             <p className='text-gray-300'>{item.info}</p>
-          </div>
-          <div className='ml-auto'>
-            {item.qrCode && <Image src={item.qrCode} alt={item.label} width={85} height={85} onClick={(e) => {
-              e.preventDefault();
-              openModal(item.qrCode);
-            }}/>}
           </div>
         </div>
         </a>
       ))}
-      <Modal isOpen={isModalOpen} onRequestClose={closeModal} qrCodeSrc={selectedQRCode || ''} />
     </div>
-  );
-};
+ )
+}
 
-export default ContactList;
-
-
-
-
+export default ContactList
