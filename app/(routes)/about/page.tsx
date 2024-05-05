10d1251754f7bcdb3bdb5170670952102ed657ae
JackSlice2 <jackslice9@gmail.com>
2024-05-05 20:17:14 -0300

    about page fixed

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -29,7 +29,7 @@ background: 'linear-gradient(160deg, #080018 15%, #2A0159 100%, #3C037F)',
         <div className=" flex flex-col justify-center items-center px-5">
       <Navbar />
       <div className="h-full w-full md:flex-row lg:flex-row xl:flex">
-        <div className="flex flex-col justify-center flex-1 w-full md:p-10 lg:px-56 xl:px-56 gap-2">
+        <div className="flex flex-col justify-center flex-1 w-full md:p-10 lg:pl-20 xl:pl-20 gap-2">
           <div className='text-center text-6xl md:text-8xl  text-medium lg:text-start xl:text-start'>
                 <h1>{AboutText.title}</h1>
                 <h1>{AboutText.subtitle}</h1>
@@ -51,7 +51,7 @@ background: 'linear-gradient(160deg, #080018 15%, #2A0159 100%, #3C037F)',
           </div>
             
             
-            <div className='gap-10 grid lg:flex xl:flex justify-center lg:mr-80 xl:mr-80 pt-32 grid-col-2'>
+            <div className='gap-10 grid lg:flex xl:flex justify-center lg:mr-40 xl:mr-32 pt-32 grid-row-2'>
               <EmployeCards 
               name='Paulo Neto (P.C)'
               specialty='Desenvolvedor Senior'
